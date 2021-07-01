e67f7a3f2ae998876bff3f78dd9ffe026e61d053
DannBlackfish <monsterjunior@outlook.es>
2021-06-30 22:24:10 -0500

    broker

--- platziverse-db/lib/metric.js
+++ platziverse-db/lib/metric.js
@@ -1,19 +1,63 @@
 'use strict'
 
-const Sequelize = require('sequelize')
-const setupDatabase = require('../lib/db')
-
-module.exports = function setupMetricModel (config) {
-  const sequelize = setupDatabase(config)
-
-  return sequelize.define('metric', {
-    type: {
-      type: Sequelize.STRING,
-      allowNull: false
-    },
-    value: {
-      type: Sequelize.TEXT,
-      allowNull: false
+module.exports = function setupAgent (AgentModel) {
+  async function createOrUpdate (agent) {
+    const cond = {
+      where: {
+        uuid: agent.uuid
+      }
     }
-  })
+
+    const existingAgent = await AgentModel.findOne(cond)
+
+    if (existingAgent) {
+      const updated = await AgentModel.update(agent, cond)
+      return updated ? AgentModel.findOne(cond) : existingAgent
+    }
+
+    const result = await AgentModel.create(agent)
+    return result.toJSON()
+  }
+
+  function findById (id) {
+    return AgentModel.findById(id)
+  }
+
+  function findByUuid (uuid) {
+    return AgentModel.findOne({
+      where: {
+        uuid
+      }
+    })
+  }
+
+  function findAll () {
+    return AgentModel.findAll()
+  }
+
+  function findConnected () {
+    return AgentModel.findAll({
+      where: {
+        connected: true
+      }
+    })
+  }
+
+  function findByUsername (username) {
+    return AgentModel.findAll({
+      where: {
+        username,
+        connected: true
+      }
+    })
+  }
+
+  return {
+    createOrUpdate,
+    findById,
+    findByUuid,
+    findAll,
+    findConnected,
+    findByUsername
+  }
 }
